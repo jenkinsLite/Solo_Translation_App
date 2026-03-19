86048104bf182d7a521ec72870930058f18a8958
mjenkins <mkjenkins101@gmail.com>
2026-03-19 13:00:09 -0400

    hooked up openai api and reformatted project for multi environments

--- index.js
+++ index.js
@@ -1,28 +1,49 @@
 const inputTxt = document.querySelector(".input-txt")
+const selectTranslation = document.querySelector(".select-translation")
 const translateBtn = document.querySelector(".translate-btn")
+const languageContainer = document.querySelector(".language-container")
+
+const origSelectTranslation = document.querySelector(".select-translation").innerHTML
+const origTranslateBtn = document.querySelector(".translate-btn").innerHTML
+const origLanguageContainer = document.querySelector(".language-container").innerHTML
 
 translateBtn.addEventListener("click", async () => {
 
-    const language = document.querySelector("input[name=language]:checked")
-    const postBody = { language: language.value, translateTxt: inputTxt.value }
+    if(!translateBtn.classList.contains("translate")) {
+        translateBtn.classList.add("translate")
+        selectTranslation.innerHTML = origSelectTranslation 
+        languageContainer.innerHTML = origLanguageContainer 
+        translateBtn.innerHTML = origTranslateBtn 
+    } else {
 
-    try {
+        translateBtn.disabled = true
 
-        const response = await fetch("/api/translate", {
-            method: 'POST',
-            headers: { "Content-Type": "application/json" }, 
-            body: JSON.stringify(postBody)
-        })
+        const language = document.querySelector("input[name=language]:checked")
+        const postBody = { language: language.value, translateTxt: inputTxt.value }
 
-        if (!response.ok) {
-            console.log("!response.ok. response is: ", response)
+        try {
+
+            const response = await fetch("/api/translate", {
+                method: 'POST',
+                headers: { "Content-Type": "application/json" }, 
+                body: JSON.stringify(postBody)
+            })
+
+            if (!response.ok) {
+                console.log("!response.ok. response is: ", response)
+            }
+            const data = await response.json()
+
+            selectTranslation.textContent = "Your translation \u{1F447}"
+            languageContainer.innerHTML = `<textarea type="text" class="output-txt">${data.request}</textarea>`
+            translateBtn.disabled = false
+            translateBtn.textContent = "Start Over"
+            translateBtn.classList.remove("translate")
+
+        } catch (error) {
+            console.log("Fetch - /api/translate Error: ", error)
         }
-        const data = await response.json()
 
-        console.log("response from server: ", data.request)
-
-    } catch (error) {
-        console.log("Fetch - /api/translate Error: ", error)
     }
 
 })
--- server/server.js
+++ server/server.js
@@ -1,18 +1,48 @@
-const express = require('express');
-const cors = require('cors');
-const path = require('path');
+import express from 'express'
+import cors from 'cors'
+import path from 'path'
+import OpenAI from 'openai'
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.use(express.static(path.join(__dirname, '../public')));
+app.use(express.static(path.join(import.meta.dirname, '../public')));
 
-app.post("/api/translate", (req, res) => {
+const openai = new OpenAI({
+    apiKey: process.env.OPENAI_API_KEY,
+    baseUrl: process.env.OPENAI_URL,
+})
 
-    console.log("got Here to the server", req.body)
+const systemPrompt = `You are a language translation expert and can translate any
+text from English to French, Spanish, or Japanese. Take any English word, statement or phrase 
+and translate it to the requested language.
 
-    res.status(200).send(JSON.stringify({request: req.body}))
+Your response must not include any introduction, conculsion, or any other text than the translation
+text.
+`
+
+app.post("/api/translate", async (req, res) => {
+
+    try {
+        const language = req.body.language
+        const userPrompt = req.body.translateTxt
+
+        const response = await openai.responses.create({
+            model: process.env.OPENAI_MODEL,
+            input: [
+                { role: 'system', content: systemPrompt },
+                { role: 'user', content: `Translate the following from English to ${language}: ${userPrompt}` }
+            ]
+        })
+
+        const translation = response.output_text
+
+        res.status(200).send(JSON.stringify({ request: translation }))
+
+    } catch (err) {
+        console.log("Something went wrong: ", err)
+    }
 })
 
 const port = process.env.PORT || 3000;
